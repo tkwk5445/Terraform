80f1d6ae36cfac8ad3b233767e0d8796f9089d28
Wookjin <131837195+tkwk5445@users.noreply.github.com>
2023-08-01 16:12:27 +0900

    Delete sg directory

--- sg/remote.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "project03_vpc" {
-    backend = "s3"
-    config = {
-        bucket = "project03-terraform-state"
-        key    = "project03/vpc/terraform.tfstate"
-        region = "ap-northeast-2"
-    }
-}
