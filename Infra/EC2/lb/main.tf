decf84b612e8ae53f7a363c0352152f1538df38c
tkwk5445 <tkwk5445@naver.com>
2023-08-03 22:00:46 +0900

    20230803

--- Infra/EC2/lb/main.tf
+++ Infra/EC2/lb/main.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-# ◀◀◀◀◀◀◀◀ 대상그룹 생성 ▶▶▶▶▶▶▶▶
+# Create Target group
 resource "aws_lb_target_group" "project03-target-group" {
   name     = "project03-target-group"
-  port     = var.server_port
+  port     = 80
   protocol = "HTTP"
   vpc_id   = data.terraform_remote_state.project03_VPC.outputs.vpc_id
 
-  # 대상그룹의 상태검사
+  
   health_check {
     path                = "/"
     protocol            = "HTTP"
@@ -19,66 +19,50 @@ resource "aws_lb_target_group" "project03-target-group" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
-
 }
-
-# ◀◀◀◀◀◀◀◀ 로드밸런서 생성 ▶▶▶▶▶▶▶▶
-resource "aws_lb" "project03-lb" {
-  name               = "project03-lb"
-  load_balancer_type = "application"
-  # 로드밸런스 생성되는 vpc의 서브넷
-  subnets = [
-    data.terraform_remote_state.project03_VPC.outputs.public_subnet2a,
-    data.terraform_remote_state.project03_VPC.outputs.public_subnet2c
-  ]
-  # 로드밸런스에 사용할 보안 그룹 
-  security_groups = [data.terraform_remote_state.project03_SG.outputs.project03-web]
-}
-# 로드밸런스에 연결할 대상그룹
-/* resource "aws_lb_target_group_attachment" "aws_lb_target_group_attachment" {
-    target_group_arn = data.terraform_remote_state.aws_lb_target_group.outputs.target_group_arn
-    target_id        = data.terraform_remote_state.aws_lb_target_group.outputs.target_group_id
-    port             = 8080
-} */
-# 로드밸런스에 연결한 대상 인스턴스
+# target to Jenkins ec2 
 resource "aws_lb_target_group_attachment" "ALB_Jenkins" {
   target_group_arn = aws_lb_target_group.project03-target-group.arn
   target_id        = data.terraform_remote_state.project03-jenkins.outputs.jenkins-EC2
   port             = 8080
 }
+# Create loadbalancer
+resource "aws_lb" "project03-lb" {
+  name               = "project03-lb"
+  load_balancer_type = "application"
+  subnets = [
+    data.terraform_remote_state.project03_VPC.outputs.public_subnet2a,
+    data.terraform_remote_state.project03_VPC.outputs.public_subnet2c
+  ]
+  security_groups = [data.terraform_remote_state.project03_SG.outputs.project03-web]
+}
 
-# ALB 리스너
+
+# ALB listener
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.project03-lb.arn
   port              = 80
   protocol          = "HTTP"
 
-  # 기본값으로 단순한 404 페이지 오류를 반환한다.
   default_action {
-    type = "fixed-response"
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.project03-target-group.arn
+  }
+}
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.project03-lb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = "arn:aws:acm:ap-northeast-2:257307634175:certificate/8238361a-128b-464c-81fe-c4108d9e5ab2"
 
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code  = 404
-    }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.project03-target-group.arn
   }
 }
 
-/* resource "aws_lb_listener" "https" {
-  load_balancer_arn       = aws_lb.project03-lb.arn
-  port                    = 443
-  protocol                = "HTTPS"
-  ssl_policy              = "ELBSecurityPolicy-2016-08"
-  certifiacertificate_arn =  */
-
-# 기본값으로 단순한 404 페이지 오류를 반환한다.
-default_action {
-  type = "forward"
-  #target_group_arn = aws_lb_target_group.project03-target-group.arn
-}
-#}
-# ALB 리스너 규칙
+# ALB listener rule
 resource "aws_lb_listener_rule" "asg" {
   listener_arn = aws_lb_listener.http.arn
 
@@ -96,31 +80,4 @@ resource "aws_lb_listener_rule" "asg" {
   }
 }
 
-/* data "aws_vpc" "project03_vpc" {
-  # 이름이 "asdf12345-vpc"인 VPC를 가져오도록 설정합니다.
-  filter {
-    name   = "tag:Name"
-    values = ["project03_vpc"]
-  }
-} */
-
-/* data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.project03_vpc.id]
-  }
-} */
-/*
-data "template_file" "web_output" {
-  template = file("${path.module}/web.sh")
-  vars = {
-    server_port = "${var.server_port}"
-  }
-}*/
-
-variable "server_port" {
-  description = "The port will use for HTTP requests"
-  type        = number
-  default     = 8080
-}
 
--- Infra/EC2/lb/remote.tf
+++ Infra/EC2/lb/remote.tf
@@ -1,10 +1,10 @@
 data "terraform_remote_state" "project03_VPC" {
-    backend = "s3"
-    config = {
-        bucket = "project03-terraform-state"
-        key    = "project03/Infra/Network/VPC/terraform.tfstate"
-        region = "ap-northeast-2"
-    }
+  backend = "s3"
+  config = {
+    bucket = "project03-terraform-state"
+    key    = "project03/Infra/Network/VPC/terraform.tfstate"
+    region = "ap-northeast-2"
+  }
 }
 
 data "terraform_remote_state" "project03_SG" {
@@ -29,34 +29,3 @@ data "terraform_remote_state" "project03-jenkins" {
   }
 }
 
-data "terraform_remote_state" "aws_lb_target_group_id" {
-
-  backend = "s3"
-  config = {
-    bucket = "project03-terraform-state"
-
-    key    = "project03/Infra/EC2/lb/terraform.tfstate"
-    region = "ap-northeast-2"
-  }
-}
-
-data "terraform_remote_state" "aws_lb_target_group_arn" {
-
-  backend = "s3"
-  config = {
-    bucket = "project03-terraform-state"
-
-    key    = "project03/Infra/EC2/lb/terraform.tfstate"
-    region = "ap-northeast-2"
-  }
-}
-/*
-data "terraform_remote_state" "launch_template" {
-  backend = "s3"
-  config = {
-    bucket = "project03-terraform-state"
-
-    key    = "project03/Infra/EC2/Launch-Template/terraform.tfstate"
-    region = "ap-northeast-2"
-  }
-}*/
