22238aafd4144dc8bcdb1fce0e99d98f69574e13
tkwk5445 <tkwk5445@naver.com>
2023-08-21 15:18:52 +0900

    Last-modified

--- S3/main.tf
+++ S3/main.tf
@@ -46,7 +46,6 @@ resource "aws_dynamodb_table" "terraform_locks" {
   name         = var.table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
-
   attribute {
     name = "LockID"
     type = "S"
